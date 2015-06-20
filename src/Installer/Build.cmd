51acb47502e16e6bfef83f0da9f3b0e4ac4122ee
mike-ward <mike@mike-ward.net>
2015-06-20 17:13:21 -0400

    Installer working

---
+++ src/Installer/Build.cmd
@@ -0,0 +1,4 @@
+del /Q *.msi
+@"c:\Program Files (x86)\WiX Toolset v3.9\bin\candle.exe" Product.wxs -dSourceFiles="..\Calendar\bin\Release" -out out\
+@if NOT ERRORLEVEL 1 "c:\Program Files (x86)\WiX Toolset v3.9\bin\light.exe" -ext WiXNetFxExtension -ext WixUIExtension out\Product.wixobj -out CalendarSetup.msi -sice:ICE61
+exit /b errorlevel
